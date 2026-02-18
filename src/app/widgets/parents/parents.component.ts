596dbd69205d2de8874687ae99bc7900eebab22e
azin <azinfirash25@gmail.com>
2026-02-18 15:48:22 +0530

    parents table

--- src/app/widgets/parents/parents.component.ts
+++ src/app/widgets/parents/parents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule }  from '@angular/common';
 
 @Component({
   selector: 'app-parents',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './parents.component.css'
 })
 export class ParentsComponent {
+   parents: any[] = [];
+
+  ngOnInit() {
+    this.loadParents();
+  }
+
+  loadParents() {
+    this.parents = JSON.parse(localStorage.getItem('parents') || '[]');
+  }
 
 }
